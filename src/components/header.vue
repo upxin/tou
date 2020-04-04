cbebb62b87a51a1a887f8505bc0ea5a0d2c85c3e
upxin <1023179834@qq.com>
2020-04-04 18:27:21 +0800

    init doc

---
+++ src/components/header.vue
@@ -0,0 +1,26 @@
+<template>
+    <section :class="b('',{a:true})">
+        <div>11</div>
+        <div></div>
+        <div></div>
+    </section>
+</template>
+
+<script>
+import create from '@/utils/create';
+
+export default create({
+    name: 'header',
+    data() {
+        return {};
+    }
+});
+
+</script>
+
+<style scoped lang='stylus'>
+.tt-header
+    width 100%
+    &--a
+        font-size 50px
+</style>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/first */
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+
+import '@/common/stylus/index.styl';
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    render: h => h(App)
+});
